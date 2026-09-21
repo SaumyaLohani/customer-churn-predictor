fdc421463f8eb3421748ef38d206726983732ca5
Saumya Lohani <72060264+SaumyaLohani@users.noreply.github.com>
2026-09-21 05:40:09 +0530

    Add files via upload

---
+++ app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ChurnPredictor from './ChurnPredictor.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChurnPredictor />
+  </React.StrictMode>,
+)
